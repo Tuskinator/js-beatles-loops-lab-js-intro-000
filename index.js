fe1728815d73421f2b512094d83b6ec51c8a8346
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-05-28 18:03:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var counter = [];
+  var len = musicians.length;
+  var i = 0;
+  for (i = 0; i < len; i++) {
+    var musicman = musicians[i]
+    var musicmech = instruments[i]
+    counter[i] = `${musicman} plays ${musicmech}`
+  };
+  return counter
+};
+function johnLennonFacts(array) {
+  var exclaim = [];
+  var length = array.length;
+  var i = 0;
+  for (i = 0; i < length; i++) {
+    var musicman = array[i]
+    exclaim[i] = `${musicman}!!!`
+  };
+  return exclaim
+};
+function iLoveTheBeatles(number) {
+  var emptyArray = [];
+  var i = 0;
+  do{
+    emptyArray[i] = "I love the Beatles!"
+  }while(number < 15);
+};
